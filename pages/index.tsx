891bcc802b95d7cb7e0de820e41ce18a42f065ae
kaique caires <kaique.caires@yahoo.com>
2021-05-10 08:09:30 -0300

    feat: make total card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello world</p>
+    <div className={styles.container}>
+      <div className={styles.total}>
+        <span className={styles.totalTitle}>Bem vindo Kaique, seu patrimônio é:</span>
+        <div className={styles.totalValue}>
+          <span className={styles.type}>R$</span>
+          <span className={styles.value}>1000,00</span>  
+        </div>
+      </div>
+
+      <div className={styles.balance}>
+        <div className={styles.income}>
+          <span className={styles.incomeTitle}>Ganhos</span>
+          <span className={styles.incomeValue}>R$ 1000,00</span>
+        </div>
+
+        <div className={styles.outcome}>
+          <span className={styles.outcomeTitle}>Gastos</span>
+          <span className={styles.outcomeValue}>R$ 1000,00</span>
+        </div>
+      </div>
+      <div className={styles.transactions}></div>
     </div>
   )
 }
