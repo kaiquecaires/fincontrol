0c34b6819e1ed9a99b626a70c74a30f1ce4cc9e7
kaique caires <kaique.caires@yahoo.com>
2021-05-11 16:04:43 -0300

    feat: make last transactions styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import styles from './home.module.scss'
+import { FaPlusCircle } from 'react-icons/fa';
+import { FiTrendingUp, FiTrendingDown } from 'react-icons/fi';
 
 export default function Home() {
   return (
@@ -13,22 +15,44 @@ export default function Home() {
 
       <div className={styles.balance}>
         <div className={styles.income}>
-          <span className={styles.title}>Ganhos</span>
+          <span className={styles.title}>
+            Ganhos
+            <FiTrendingUp color="#74DCC5" />
+          </span>
           <div className={styles.valueContainer}>
             <span className={styles.type}>R$</span>
             <span className={styles.value}>1000,00</span>  
           </div>
         </div>
-
         <div className={styles.outcome}>
-          <span className={styles.title}>Gastos</span>
+          <span className={styles.title}>
+            Gastos
+            <FiTrendingDown color="#CF6679" />
+          </span>
           <div className={styles.valueContainer}>
             <span className={styles.type}>R$</span>
             <span className={styles.value}>1000,00</span>  
           </div>
         </div>
       </div>
-      <div className={styles.transactions}></div>
+
+      <div className={styles.transactionsContainer}>
+        <div className={styles.transactionsHeader}>
+          <span className={styles.transactionsTitle}>Últimas transações</span>
+          <FaPlusCircle color="#74DCC5" size={30} />
+        </div>
+        <div className={styles.transactions}>
+          <div className={styles.transaction}>
+            <span className={styles.transactionTitle}>Compras</span>
+            <span className={styles.transactionOutcome}>- R$ 1000,00</span>
+          </div>
+
+          <div className={styles.transaction}>
+            <span className={styles.transactionTitle}>Salário</span>
+            <span className={styles.transactionIncome}>R$ 2000,00</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
